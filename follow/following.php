b5c09025d9e3e44288b2829b6dde8712a3c383cf
Rizal <syahrizalhanif@gmail.com>
2023-01-05 19:58:35 +0700

    Create following API

---
+++ follow/following.php
@@ -0,0 +1,17 @@
+<?php
+include '../connection.php';
+
+$from_id_user = $_POST['from_id_user'];
+$to_id_user = $_POST['to_id_user'];
+
+$sql = "INSERT INTO following SET from_id_user = '$from_id_user', to_id_user = '$to_id_user'";
+$result = $conn->query($sql);
+if ($result) {
+    echo json_encode(array(
+        'success' => true,
+    ));
+} else {
+    echo json_encode(array(
+        'success' => false,
+    ));
+}
