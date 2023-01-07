0f7979d0e8b885fbc6ed565108f8d6021470034b
Rizal <syahrizalhanif@gmail.com>
2023-01-07 23:18:01 +0700

    Create read where id user topic API

---
+++ topic/read_where_id_user.php
@@ -0,0 +1,22 @@
+<?php
+include '../connection.php';
+
+$id_user = $_POST['id_user'];
+
+$sql = "SELECT * FROM topic WHERE id_user = '$id_user'";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    $data = array();
+    while($get_row = $result->fetch_assoc()) {
+        $data[] = $get_row;
+    }
+    echo json_encode(array(
+        'success' => true,
+        'data' => $data
+    ));
+} else {
+    echo json_encode(array(
+        'success' => false,
+        'data' => []
+    ));
+}
