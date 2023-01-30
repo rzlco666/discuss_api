4e2f24942acd6c5f6ec73f035a834d7981fd6999
Rizal <syahrizalhanif@gmail.com>
2023-01-30 11:15:41 +0700

    Fix table name for following

--- follow/following.php
+++ follow/following.php
@@ -4,7 +4,7 @@ include '../connection.php';
 $from_id_user = $_POST['from_id_user'];
 $to_id_user = $_POST['to_id_user'];
 
-$sql = "INSERT INTO following SET from_id_user = '$from_id_user', to_id_user = '$to_id_user'";
+$sql = "INSERT INTO follow SET from_id_user = '$from_id_user', to_id_user = '$to_id_user'";
 $result = $conn->query($sql);
 if ($result) {
     echo json_encode(array(
